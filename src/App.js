21112daeba8131cce925122187b53f1bfebc70a6
sonam <tseringsonam34@hotmail.com>
2022-08-22 18:33:10 -0400

    used object given to me

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import data from './data.json'
 import './App.css';
 
 function App() {
+  const datas =  data.map(info => {
+    return(
+      <div>
+        <p>{info.question}</p>
+        <p>{info.answer}</p>
+      </div> 
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='inner-div'>
+        <h1>Frequently Asked Questions</h1>
+        {datas}
+      </div>
     </div>
   );
 }
