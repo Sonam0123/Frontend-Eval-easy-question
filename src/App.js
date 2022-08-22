77bd9a0075408f3b7855aa363071278a8704a47a
sonam <tseringsonam34@hotmail.com>
2022-08-22 19:15:03 -0400

    mapped questions and answers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import data from './data.json'
 import './App.css';
+import { BsFillArrowRightSquareFill } from 'react-icons/bs';
 
 function App() {
+
+  
   const datas =  data.map(info => {
     return(
-      <div>
-        <p>{info.question}</p>
+      <div className='q-and-a'>
+        <p> <BsFillArrowRightSquareFill /> {info.question}</p>
         <p>{info.answer}</p>
       </div> 
     )
   })
   return (
     <div className="App">
-      <div className='inner-div'>
-        <h1>Frequently Asked Questions</h1>
-        {datas}
+      <div className='outer-div'>
+          <h1>Frequently Asked Questions</h1>
+        <div className='inner-div'>
+          {datas}
+        </div>
       </div>
     </div>
   );
